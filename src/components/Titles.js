6eae48de4f6307a5709990761cf184369a34606f
Md Sohanur Rahman JoY <md.sohanur.rahman.joy@gmail.com>
2018-07-06 02:15:52 +0600

    First commit

---
+++ src/components/Titles.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Titles = () => (
+    <div>
+        <h1 className="title-container__title">Weather App </h1>
+        <h3>Manchester & London (US, UK)</h3>
+        <h4 className="title-container__subtitle">Find out temperature, conditions and more</h4>
+    </div>
+);
+
+export default Titles;
